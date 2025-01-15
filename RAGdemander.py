bd7c5f29ef669bcd5ed7528b46d8b8e64370d725
SaturnTsen <minger233@outlook.com>
2025-01-15 14:36:40 +0100

    Update docs

--- RAGdemander.py
+++ RAGdemander.py
@@ -3,12 +3,26 @@ from openai import OpenAI
 from myTools import read_file
 import os
 docu = read_file(os.path.join("docs", "envision-brief.md"))
-demander_personality = "You are a coder's assistant. The coder will be coding in a Domain Specific Language called Envision. You are given the BASIC DOCUMENTATION of Envision：\n&BASIC DOCUMENTATION"+docu + \
-    "END OF BASIC DOCUMENTATION\n&\nThe user will give you a CODING TASK as input. You shall not try to code yourself; instead, based on the BASIC DOCUMENTATION you have seen above, you shall propose some grammar points or function usage that are NOT specified in the BASIC DOCUMENTATION but that a coder will nevertheless need to know in order to complete the CODING TASK. Output your answer as one or more concise bulletpoints, like [- the 'cos' function'] or [- handle dates] or [- defining a 'zedfunc']. Do not give any intermediate thinking or explain what you need the information for."
+demander_personality = "You are a coder's assistant. The coder will be coding in a Domain Specific Language called Envision. You are given the BASIC DOCUMENTATION of Envision: \n&BASIC DOCUMENTATION"+docu + \
+    "END OF BASIC DOCUMENTATION\n&\nThe user will give you a CODING TASK as input. You shall not try to code yourself; instead, based on the BASIC DOCUMENTATION you have seen above, you shall propose some grammar points or function usage that are NOT specified in the BASIC DOCUMENTATION but that a coder will nevertheless need to know in order to complete the CODING TASK. Output your answer as one or more concise bullet points, like [- the 'cos' function'] or [- handle dates] or [- defining a 'zedfunc']. Do not give any intermediate thinking or explain what you need the information for."
 client = OpenAI(api_key=api_key)
 
 
 def RAGdemand(question, demander_personality=demander_personality):
+    """
+    Generates a response to a given question using the demander's personality.
+    Specifically, the demander is asked to propose some grammar points or
+    function usage that are NOT specified in the BASIC DOCUMENTATION but that a
+    coder will nevertheless need to know in order to complete the CODING TASK.
+    The output of the demander is a list of bullet points, e.g. [- the 'cos'
+    function'] or [- handle dates] or [- defining a 'zedfunc']. Args:
+        question (str): The question to be answered. demander_personality (str):
+        The personality to be used for generating the response.
+
+    Returns:
+        list: A list of strings containing the response, split by newline
+        characters, containing the proposed grammar points or function usage.
+    """
     demander_response = client.chat.completions.create(
         model='gpt-4o',
         messages=[
--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 ### This reposit is for the group project in Ecole Polytechnique. ###
 ### Please do not use or download the files in this reposit if you are not involved with the project, thank you ! ###
 
+
 Newly done:
 LLM RAGdemander, it is now connected to RAGsearcher  
 results: RAGdemander ok in proposing relevant ideas, but the transformer cannot robustly match to the paragraph.  
@@ -14,3 +15,34 @@ light method: try only matching text, neglecting envision code when encoding par
 5. Change model / transformer to allow for precise matching with long paragraphs  
 6. After a 1st output, if does not pass compilation, retract doc for each error point. (how to detect?)     
 7. After a 1st batch retrieved, make a 2nd retrieval based on the 1st-batch material?
+
+## Main entries
+
+`LLMasJudge.py`
+
+```
+- pipeline_verify:
+    1. Generate a student's response to a given challenge.
+    2. Check the response for compilation errors. if it does not compile, go back to step 1.
+    2. Verity the response using a judge.
+    3. Then a verifier converts the judge's decision to a binary output.
+
+- pipeline_score_allchallenge:
+    Test on a list of challenges by verifying each one using the pipeline_verify function.
+    It prints the number of correct responses and the overall percentage accuracy.
+```
+
+`RAGsearcher.py`
+
+```
+This module provides functionality to retrieve relevant documentation based on a
+given question and generate responses using a RAG pipeline. Functions:
+    feed_to_RAG(question):
+        Loads documentation and reference texts, and generates text to feed into
+        the RAG model based on the input question.
+    RAG_pipeline(question, coder_personality=RAGcoder_personality):
+        Enhances the input question using RAGdemand, retrieves relevant
+        documentation, and generates a response using the RAG model.
+```
+
+Question: These two functionalities are not yet merged?
