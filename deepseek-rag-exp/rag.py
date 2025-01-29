de37a4c1258261d63dd3262bc514ba249a99fe16
SaturnTsen <minger233@outlook.com>
2025-01-29 17:24:53 +0100

    123

--- deepseek-rag-exp/rag.py
+++ deepseek-rag-exp/rag.py
@@ -3,6 +3,12 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import DirectoryLoader
 from config import CONFIG
 
+#%% Deos
+#!pip install unstructured
+#!pip install -qU langchain_chroma
+#!pip install python-magic python-magic-bin
+#!pip install -qU langchain_ollama
+
 #%% load docs
 loader = DirectoryLoader(CONFIG.TUTO_PATH, glob="**/*.md")
 documents = loader.load()
@@ -15,3 +21,9 @@ docs = text_splitter.split_documents(documents)
 print(f"Loaded {len(docs)} document chunks")
 
 # %%
+from langchain_chroma import Chroma
+from langchain_ollama import OllamaEmbeddings
+
+local_embeddings = OllamaEmbeddings(model="nomic-embed-text")
+
+vectorstore = Chroma.from_documents(documents=all_splits, embedding=local_embeddings)
